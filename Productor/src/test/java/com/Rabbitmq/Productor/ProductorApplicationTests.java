f64f4b82799e5e64de90c3fea3920e9a46477c65
Nicolás Rincón <ndrinconc@gmail.com>
2023-11-15 17:01:11 -0500

    Initial commit

---
+++ Productor/src/test/java/com/Rabbitmq/Productor/ProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rabbitmq.Productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
