92076896519cee8bb3a8a36fb4545b5d9ee3482b
Nicolás Rincón <ndrinconc@gmail.com>
2023-11-15 22:42:02 -0500

    Arregla Problemas

--- Productor/src/main/java/com/Rabbitmq/Productor/SenderController.java
+++ Productor/src/main/java/com/Rabbitmq/Productor/SenderController.java
@@ -1,19 +1,18 @@
 package com.Rabbitmq.Productor;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Random;
-import java.util.logging.Logger;
 
 @Service
 public class SenderController {
 
-    private static final Logger log = (Logger) LoggerFactory.getLogger(SenderController.class);
+    private static final Logger log = LoggerFactory.getLogger(SenderController.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -21,7 +20,8 @@ public class SenderController {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    @Scheduled(fixedDelay = 2000L)
+
+    @Scheduled(fixedDelay = 3000L)
     public void sendData() {
         MessageController tip = new MessageController("Transfer received and sent successfully.", new Random().nextInt(100), false, LocalDateTime.now() );
         rabbitTemplate.convertAndSend(ProductorApplication.EXCHANGE_NAME, ProductorApplication.ROUTING_KEY, tip);
